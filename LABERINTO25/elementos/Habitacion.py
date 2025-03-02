448012276599e74b218e0c221d34f24d1ce1f156
mandp9 <MPilar.Ballesteros3@alu.uclm.es>
2025-03-02 13:11:41 +0100

    Implementados Habitacion, el __init__ de elemento mapa y añadida clase Laberinto

---
+++ LABERINTO25/elementos/Habitacion.py
@@ -0,0 +1,51 @@
+from .Contenedor import Contenedor
+
+class Habitacion(Contenedor):
+    """Representa una habitación dentro del laberinto."""
+
+    def __init__(self, num):
+        super().__init__()  # Llama al constructor de Contenedor
+        self.num = num  # Identificador único de la habitación
+        self.norte = None
+        self.sur = None
+        self.este = None
+        self.oeste = None
+    
+    def entrar(self):
+        """Método sobrescrito de 'ElementoMapa' para indicar que se ha entrado en la habitación."""
+        print(f"Estás en la habitación {self.num}.")
+
+    def es_habitacion(self):
+        return True
+
+    @property
+    def norte(self):
+        return self._norte
+
+    @norte.setter
+    def norte(self, nueva_habitacion):
+        self._norte = nueva_habitacion
+    
+    @property
+    def sur(self):
+        return self._sur
+
+    @sur.setter
+    def sur(self, nueva_habitacion):
+        self._sur = nueva_habitacion
+    
+    @property
+    def este(self):
+        return self._este
+
+    @este.setter
+    def este(self, nueva_habitacion):
+        self._este = nueva_habitacion
+
+    @property
+    def oeste(self):
+        return self._oeste
+
+    @oeste.setter
+    def oeste(self, nueva_habitacion):
+        self._oeste = nueva_habitacion
---
+++ LABERINTO25/elementos/__init__.py
@@ -0,0 +1,7 @@
+from .ElementoMapa import ElementoMapa
+from .Contenedor import Contenedor
+from .Habitacion import Habitacion
+from .Orientacion import Orientacion
+from .Pared import Pared
+from .Puerta import Puerta
+from .Laberinto import Laberinto
