3fdb246aaae7f48488ea6bfdf89ce1ad8e3c14b3
mandp9 <MPilar.Ballesteros3@alu.uclm.es>
2025-03-02 20:02:47 +0100

    Implementada ParedBomba

---
+++ LABERINTO25/decoradores/Bomba.py
@@ -0,0 +1,4 @@
+from elementos.Decorator import Decorator
+
+class Bomba(Decorator):
+    """Decorador que representa una bomba en el laberinto."""
---
+++ LABERINTO25/elementos/ParedBomba.py
@@ -0,0 +1,24 @@
+from .Pared import Pared
+
+class ParedBomba(Pared):
+    """Pared especial que explota cuando alguien intenta cruzarla."""
+    def __init__(self, activa=False):
+        super().__init__()
+        self._activa = activa
+    
+    @property
+    def activa(self):
+        return self._activa
+
+    @activa.setter
+    def activa(self, estado):
+        if isinstance(estado, bool):
+            self._activa = estado
+        else:
+            raise TypeError("El estado de 'activa' debe ser un booleano.")
+
+    def entrar(self):
+        if self._activa:
+            print("💥 ¡BOOM! Has chocado con una pared bomba y ha explotado.")  # Simula el Transcript
+        else:
+            print("🚧 Has chocado con una pared bomba, pero no está activa.") 
